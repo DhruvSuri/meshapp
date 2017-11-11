26f3966b1090225fea95ca9811eea41d4414e568
Dhruv Suri <Dhruv>
2017-11-11 12:29:36 +0530

    Threhold test

--- src/main/java/in/meshworks/utils/AzazteUtils.java
+++ src/main/java/in/meshworks/utils/AzazteUtils.java
@@ -1,6 +1,7 @@
 package in.meshworks.utils;
 
 import com.google.gson.Gson;
+import in.meshworks.beans.Res;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -36,29 +37,41 @@ public class AzazteUtils {
         int i = 0;
         Map<String, Integer> map = new HashMap();
         int proxyCount = 0;
-        while(i < 100){
-            try {
-                i++;
-                OkHttpClient client = new OkHttpClient();
+        while (i < 100) {
+            i++;
+            new Thread() {
+                public void run() {
+                    try {
+                        OkHttpClient client = new OkHttpClient();
 
-                Request request = new Request.Builder()
-                        .url("http://13.126.226.17/proxy?url=http://checkip.amazonaws.com")
-                        .build();
-                Response execute = client.newCall(request).execute();
-                String ip = execute.body().string();
-                Integer count = map.get(ip);
-                if (count == null){
-                    count = 0;
+
+                        Request request = new Request.Builder()
+                                .url("https://wnsly2hzi6.execute-api.ap-south-1.amazonaws.com/Prod?url=http://checkip.amazonaws.com&type=0")
+                                .build();
+                        Response execute = client.newCall(request).execute();
+                        synchronized (map){
+                            String ip = execute.body().string();
+                            Integer count = map.get(ip);
+                            if (count == null) {
+                                count = 0;
+                            }
+                            map.put(ip, count + 1);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
-                proxyCount ++;
-                map.put(ip, count + 1);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            }.start();
         }
 
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println(map.toString());
-        System.out.println(proxyCount);
+        System.out.println("Total Count " + proxyCount);
+        System.out.println("Unique Count" + map.keySet().size());
 
     }
 }
