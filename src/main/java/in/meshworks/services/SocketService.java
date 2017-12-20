ad8383b80d06ee5ab73bacf9f6d1a89671f89f5d
Harsh Vardhan Sharma <bonnyharsh1993@gmail.com>
2017-12-20 19:51:28 +0530

    increased worker threads to 3k

--- src/main/java/in/meshworks/services/SocketService.java
+++ src/main/java/in/meshworks/services/SocketService.java
@@ -51,7 +51,7 @@ public class SocketService {
                 config.setUpgradeTimeout(10000000);
                 config.setMaxFramePayloadLength(Integer.MAX_VALUE);
                 config.setMaxHttpContentLength(Integer.MAX_VALUE);
-                config.setWorkerThreads(1000);
+                config.setWorkerThreads(3000);
 
                 config.setPort(defaultPort);
                 socketConfig.setReuseAddress(true);
